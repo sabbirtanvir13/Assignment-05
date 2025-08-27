ca3817d035ac0cae1ec5901810276ab6ba6b44c5
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-08-27 18:45:18 +0600

    all section

---
+++ script.js
@@ -0,0 +1,18 @@
+// function getElement(id){
+//     const Element=document.getElementById(id);
+//     return Element
+// }
+// document.getElementById('btn-1').addEventListener('click',function(){
+//     for(let i ){}
+// })
+
+
+// 
+
+//   document.getElementById('heart-1').addEventListener('click', function (e) {
+//     e.preventDefault()
+//     const total=1
+//     const love = parseInt(document.getElementById('love-1').innerText);
+//     const totallove =total+love;
+//     document.getElementById('love-1').innerText = totallove;
+//   });
