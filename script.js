01cf3debc2aff2af99f25d386c1eb4cee147d4cb
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-08-27 21:59:17 +0600

    love added

--- script.js
+++ script.js
@@ -1,18 +1,104 @@
-// function getElement(id){
-//     const Element=document.getElementById(id);
-//     return Element
-// }
-// document.getElementById('btn-1').addEventListener('click',function(){
-//     for(let i ){}
-// })
 
 
-// 
+// heart 1
+  document.getElementById("heart-1").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 1
+    const love = parseInt(document.getElementById("love-1").innerText);
+    const totallove =total+love;
+    document.getElementById("love-1").innerText = totallove;
+   
+  });
+// heart 2
+  document.getElementById("heart-2").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 1
+    const love = parseInt(document.getElementById("love-1").innerText);
+    const totallove =total+love;
+    document.getElementById("love-1").innerText = totallove;
+   
+  });
+// heart 3
+  document.getElementById("heart-3").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 1
+    const love = parseInt(document.getElementById("love-1").innerText);
+    const totallove =total+love;
+    document.getElementById("love-1").innerText = totallove;
+   
+  });
+// heart 4
+  document.getElementById("heart-4").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 1
+    const love = parseInt(document.getElementById("love-1").innerText);
+    const totallove =total+love;
+    document.getElementById("love-1").innerText = totallove;
+   
+  });
+// heart 5
+  document.getElementById("heart-5").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 1
+    const love = parseInt(document.getElementById("love-1").innerText);
+    const totallove =total+love;
+    document.getElementById("love-1").innerText = totallove;
+   
+  });
+// heart 6
+  document.getElementById("heart-6").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 1
+    const love = parseInt(document.getElementById("love-1").innerText);
+    const totallove =total+love;
+    document.getElementById("love-1").innerText = totallove;
+   
+  });
+// heart 7
+  document.getElementById("heart-7").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 1
+    const love = parseInt(document.getElementById("love-1").innerText);
+    const totallove =total+love;
+    document.getElementById("love-1").innerText = totallove;
+   
+  });
+// heart 8
+  document.getElementById("heart-8").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 1
+    const love = parseInt(document.getElementById("love-1").innerText);
+    const totallove =total+love;
+    document.getElementById("love-1").innerText = totallove;
+   
+  });
+// heart 9
+  document.getElementById("heart-9").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 1
+    const love = parseInt(document.getElementById("love-1").innerText);
+    const totallove =total+love;
+    document.getElementById("love-1").innerText = totallove;
+   
+  });
 
-//   document.getElementById('heart-1').addEventListener('click', function (e) {
-//     e.preventDefault()
-//     const total=1
-//     const love = parseInt(document.getElementById('love-1').innerText);
-//     const totallove =total+love;
-//     document.getElementById('love-1').innerText = totallove;
-//   });
+
+//   coin 
+ document.getElementById("btn-1").addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("bangla bhai");
+    const total= 20
+    const coineall = parseInt(document.getElementById("coine").innerText);
+    const totalcoine =total-coineall;
+    document.getElementById("coine").innerText = totalcoine;
+   
+  });
