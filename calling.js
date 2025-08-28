fe79ab0b76b75ae713a7ea6d2aa695fcc9bf29d3
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-08-28 15:53:45 +0600

    call and added call history

---
+++ calling.js
@@ -0,0 +1,264 @@
+    // bt-1
+      document.getElementById("btn-1").addEventListener('click', function (e) {
+        e.preventDefault();
+        const coineElement = document.getElementById("coine");
+        let coineall = parseInt(coineElement.innerText);
+        if (coineall >= 20) {  
+          coineall -= 20; 
+          coineElement.innerText = coineall;
+        //   time or date
+          const now = new Date();
+          const timeString = now.toLocaleString(); 
+          // cart container
+          const cartContainer = document.getElementById("cart-container");
+          // new div make
+          const newcart = document.createElement("div");
+          newcart.classList.add("m-2", "bg-gray-200", "p-3", "rounded-lg");
+          newcart.innerHTML = `
+              <h1 class="font-semibold">Fire Service Number</h1>
+              <p>999</p>
+              <h1 class="text-gray-600 flex justify-between font-semibold"> Time: ${timeString}</h1>
+          `;
+          // add to history
+          cartContainer.appendChild(newcart);
+          alert(' Calling Emergency Number 999...');
+        } else {
+          alert("Low coin balance!");
+        }
+      });
+    // btn-2
+      document.getElementById("btn-2").addEventListener('click', function (e) {
+        e.preventDefault();
+        const coineElement = document.getElementById("coine");
+        let coineall = parseInt(coineElement.innerText);
+        if (coineall >= 20) {  
+          coineall -= 20; 
+          coineElement.innerText = coineall;
+        //   time or date
+          const now = new Date();
+          const timeString = now.toLocaleString(); 
+          // cart container
+          const cartContainer = document.getElementById("cart-container");
+          // new div make
+          const newcart = document.createElement("div");
+          newcart.classList.add("m-2", "bg-gray-200", "p-3", "rounded-lg");
+          newcart.innerHTML = `
+              <h1 class="font-semibold">Police Helpline Number</h1>
+              <p>999</p>
+              <h1 class="text-gray-600 flex justify-between font-semibold"> Time: ${timeString}</h1>
+          `;
+          // add to history
+          cartContainer.appendChild(newcart);
+          alert(' Calling Emergency Number 999...');
+        } else {
+          alert("Low coin balance!");
+        }
+      });
+
+    // btn-3
+      document.getElementById("btn-3").addEventListener('click', function (e) {
+        e.preventDefault();
+        const coineElement = document.getElementById("coine");
+        let coineall = parseInt(coineElement.innerText);
+        if (coineall >= 20) {  
+          coineall -= 20; 
+          coineElement.innerText = coineall;
+        //   time or date
+          const now = new Date();
+          const timeString = now.toLocaleString(); 
+          // cart container
+          const cartContainer = document.getElementById("cart-container");
+          // new div make
+          const newcart = document.createElement("div");
+          newcart.classList.add("m-2", "bg-gray-200", "p-3", "rounded-lg");
+          newcart.innerHTML = `
+              <h1 class="font-semibold">Fire Service Number</h1>
+              <p>999</p>
+              <h1 class="text-gray-600 flex justify-between font-semibold"> Time: ${timeString}</h1>
+          `;
+          // add to history
+          cartContainer.appendChild(newcart);
+          alert(' Calling Emergency Number 999...');
+        } else {
+          alert("Low coin balance!");
+        }
+      });
+
+    // btn-4
+      document.getElementById("btn-4").addEventListener('click', function (e) {
+        e.preventDefault();
+        const coineElement = document.getElementById("coine");
+        let coineall = parseInt(coineElement.innerText);
+        if (coineall >= 20) {  
+          coineall -= 20; 
+          coineElement.innerText = coineall;
+        //   time or date
+          const now = new Date();
+          const timeString = now.toLocaleString(); 
+          // cart container
+          const cartContainer = document.getElementById("cart-container");
+          // new div make
+          const newcart = document.createElement("div");
+          newcart.classList.add("m-2", "bg-gray-200", "p-3", "rounded-lg");
+          newcart.innerHTML = `
+              <h1 class="font-semibold">Ambulance Service</h1>
+              <p>1994-999999
+               </p>
+              <h1 class="text-gray-600 flex justify-between font-semibold"> Time: ${timeString}</h1>
+          `;
+          // add to history
+          cartContainer.appendChild(newcart);
+          alert(' Calling Emergency Number 999...');
+        } else {
+          alert("Low coin balance!");
+        }
+      });
+
+    // btn-5
+      document.getElementById("btn-5").addEventListener('click', function (e) {
+        e.preventDefault();
+        const coineElement = document.getElementById("coine");
+        let coineall = parseInt(coineElement.innerText);
+        if (coineall >= 20) {  
+          coineall -= 20; 
+          coineElement.innerText = coineall;
+        //   time or date
+          const now = new Date();
+          const timeString = now.toLocaleString(); 
+          // cart container
+          const cartContainer = document.getElementById("cart-container");
+          // new div make
+          const newcart = document.createElement("div");
+          newcart.classList.add("m-2", "bg-gray-200", "p-3", "rounded-lg");
+          newcart.innerHTML = `
+              <h1 class="font-semibold">Women & Child Helpline</h1>
+              <p>109</p>
+              <h1 class="text-gray-600 flex justify-between font-semibold"> Time: ${timeString}</h1>
+          `;
+          // add to history
+          cartContainer.appendChild(newcart);
+          alert(' Calling Emergency Number 999...');
+        } else {
+          alert("Low coin balance!");
+        }
+      });
+
+    // btn-6
+      document.getElementById("btn-6").addEventListener('click', function (e) {
+        e.preventDefault();
+        const coineElement = document.getElementById("coine");
+        let coineall = parseInt(coineElement.innerText);
+        if (coineall >= 20) {  
+          coineall -= 20; 
+          coineElement.innerText = coineall;
+        //   time or date
+          const now = new Date();
+          const timeString = now.toLocaleString(); 
+          // cart container
+          const cartContainer = document.getElementById("cart-container");
+          // new div make
+          const newcart = document.createElement("div");
+          newcart.classList.add("m-2", "bg-gray-200", "p-3", "rounded-lg");
+          newcart.innerHTML = `
+              <h1 class="font-semibold">Anti-Corruption Helpline</h1>
+              <p>106</p>
+              <h1 class="text-gray-600 flex justify-between font-semibold"> Time: ${timeString}</h1>
+          `;
+          // add to history
+          cartContainer.appendChild(newcart);
+          alert(' Calling Emergency Number 999...');
+        } else {
+          alert("Low coin balance!");
+        }
+      });
+
+    // btn-7
+      document.getElementById("btn-7").addEventListener('click', function (e) {
+        e.preventDefault();
+        const coineElement = document.getElementById("coine");
+        let coineall = parseInt(coineElement.innerText);
+        if (coineall >= 20) {  
+          coineall -= 20; 
+          coineElement.innerText = coineall;
+        //   time or date
+          const now = new Date();
+          const timeString = now.toLocaleString(); 
+          // cart container
+          const cartContainer = document.getElementById("cart-container");
+          // new div make
+          const newcart = document.createElement("div");
+          newcart.classList.add("m-2", "bg-gray-200", "p-3", "rounded-lg");
+          newcart.innerHTML = `
+              <h1 class="font-semibold">Electricity Helpline</h1>
+              <p>16216</p>
+              <h1 class="text-gray-600 flex justify-between font-semibold"> Time: ${timeString}</h1>
+          `;
+          // add to history
+          cartContainer.appendChild(newcart);
+          alert(' Calling Emergency Number 999...');
+        } else {
+          alert("Low coin balance!");
+        }
+      });
+
+    // btn-8
+      document.getElementById("btn-8").addEventListener('click', function (e) {
+        e.preventDefault();
+        const coineElement = document.getElementById("coine");
+        let coineall = parseInt(coineElement.innerText);
+        if (coineall >= 20) {  
+          coineall -= 20; 
+          coineElement.innerText = coineall;
+        //   time or date
+          const now = new Date();
+          const timeString = now.toLocaleString(); 
+          // cart container
+          const cartContainer = document.getElementById("cart-container");
+          // new div make
+          const newcart = document.createElement("div");
+          newcart.classList.add("m-2", "bg-gray-200", "p-3", "rounded-lg");
+          newcart.innerHTML = `
+              <h1 class="font-semibold">Brac Helpline</h1>
+              <p>16445</p>
+              <h1 class="text-gray-600 flex justify-between font-semibold"> Time: ${timeString}</h1>
+          `;
+          // add to history
+          cartContainer.appendChild(newcart);
+          alert(' Calling Emergency Number 999...');
+        } else {
+          alert("Low coin balance!");
+        }
+      });
+
+    // btn-9
+      document.getElementById("btn-9").addEventListener('click', function (e) {
+        e.preventDefault();
+        const coineElement = document.getElementById("coine");
+        let coineall = parseInt(coineElement.innerText);
+        if (coineall >= 20) {  
+          coineall -= 20; 
+          coineElement.innerText = coineall;
+        //   time or date
+          const now = new Date();
+          const timeString = now.toLocaleString(); 
+          // cart container
+          const cartContainer = document.getElementById("cart-container");
+          // new div make
+          const newcart = document.createElement("div");
+          newcart.classList.add("m-2", "bg-gray-200", "p-3", "rounded-lg");
+          newcart.innerHTML = `
+              <h1 class="font-semibold">Bangladesh Railway Helpline</h1>
+              <p>163</p>
+              <h1 class="text-gray-600 flex justify-between font-semibold"> Time: ${timeString}</h1>
+          `;
+          // add to history
+          cartContainer.appendChild(newcart);
+          alert(' Calling Emergency Number 999...');
+        } else {
+          alert("Low coin balance!");
+        }
+      });
+
+
+
+  
--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // heart 1
   document.getElementById("heart-1").addEventListener('click', function (e) {
     e.preventDefault()
-    console.log("bangla bhai");
+  
     const total= 1
     const love = parseInt(document.getElementById("love-1").innerText);
     const totallove =total+love;
@@ -13,7 +13,7 @@
 // heart 2
   document.getElementById("heart-2").addEventListener('click', function (e) {
     e.preventDefault()
-    console.log("bangla bhai");
+   
     const total= 1
     const love = parseInt(document.getElementById("love-1").innerText);
     const totallove =total+love;
@@ -23,7 +23,7 @@
 // heart 3
   document.getElementById("heart-3").addEventListener('click', function (e) {
     e.preventDefault()
-    console.log("bangla bhai");
+    
     const total= 1
     const love = parseInt(document.getElementById("love-1").innerText);
     const totallove =total+love;
@@ -33,7 +33,7 @@
 // heart 4
   document.getElementById("heart-4").addEventListener('click', function (e) {
     e.preventDefault()
-    console.log("bangla bhai");
+   
     const total= 1
     const love = parseInt(document.getElementById("love-1").innerText);
     const totallove =total+love;
@@ -43,7 +43,7 @@
 // heart 5
   document.getElementById("heart-5").addEventListener('click', function (e) {
     e.preventDefault()
-    console.log("bangla bhai");
+   
     const total= 1
     const love = parseInt(document.getElementById("love-1").innerText);
     const totallove =total+love;
@@ -53,7 +53,7 @@
 // heart 6
   document.getElementById("heart-6").addEventListener('click', function (e) {
     e.preventDefault()
-    console.log("bangla bhai");
+    
     const total= 1
     const love = parseInt(document.getElementById("love-1").innerText);
     const totallove =total+love;
@@ -63,7 +63,7 @@
 // heart 7
   document.getElementById("heart-7").addEventListener('click', function (e) {
     e.preventDefault()
-    console.log("bangla bhai");
+    
     const total= 1
     const love = parseInt(document.getElementById("love-1").innerText);
     const totallove =total+love;
@@ -73,7 +73,7 @@
 // heart 8
   document.getElementById("heart-8").addEventListener('click', function (e) {
     e.preventDefault()
-    console.log("bangla bhai");
+  
     const total= 1
     const love = parseInt(document.getElementById("love-1").innerText);
     const totallove =total+love;
@@ -83,22 +83,32 @@
 // heart 9
   document.getElementById("heart-9").addEventListener('click', function (e) {
     e.preventDefault()
-    console.log("bangla bhai");
+   
     const total= 1
     const love = parseInt(document.getElementById("love-1").innerText);
     const totallove =total+love;
     document.getElementById("love-1").innerText = totallove;
    
-  });
+  }); 
 
+  
+           // copy 
+ 
+  document.getElementById("copy-1").addEventListener('click', function (e) {
+    e.preventDefault();
+     
+    const copytotal = 1;
+    const copyiteam = parseInt(document.getElementById("copycount1").innerText);
+    const totalcopyiteam = copytotal + copyiteam;
+    document.getElementById("copycount1").innerText = totalcopyiteam;
+    alert(" Card number is now copied 999..");
+      const cardText = document.getElementById("cardNumber").innerText;
+    navigator.clipboard.writeText(cardText);
 
-//   coin 
- document.getElementById("btn-1").addEventListener('click', function (e) {
-    e.preventDefault()
-    console.log("bangla bhai");
-    const total= 20
-    const coineall = parseInt(document.getElementById("coine").innerText);
-    const totalcoine =total-coineall;
-    document.getElementById("coine").innerText = totalcoine;
    
   });
+
+   
+  
+  
+  
